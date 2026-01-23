2540433e3e2fcf1308ae0b1e1ff3087ed7e1c1b6
pedrovalcarcel <pedrusqui777@gmail.com>
2026-01-23 12:34:15 +0100

    partidos_mes.js

--- partidos_mes.js
+++ partidos_mes.js
@@ -3,10 +3,12 @@ fetch("resultados.json")
 .then(data => {
     const ul = document.getElementById("partidos-mes");
     const mesActual = new Date().getMonth();
+    const diaHoy = new Date().getDate();
+    const anoActual = new Date().getFullYear();
 
     data.partidos.forEach(p => {
         const fecha = new Date(p.fecha.split("/").reverse().join("-"));
-        if (fecha.getMonth() === (mesActual)) {
+        if (fecha.getMonth() === (mesActual) && fecha.getUTCDate() > diaHoy && anoActual <= fecha.getFullYear() || fecha.getMonth() > (mesActual) & anoActual <= fecha.getFullYear()) {
             const li = document.createElement("li");
             li.innerHTML = `
             <a href="partido.html?id=${p.id}">
@@ -23,3 +25,5 @@ function obtenerDiaSemana(fecha) {
   const date = new Date(ano, mes - 1, dia); // Crea un objeto Date a partir de la fecha
   return dias[date.getDay()]; // Devuelve el nombre del día
   }
+
+
